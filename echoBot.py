bbff0c9872d098a06aa1d1b6e9af5b219c5bf854
Justus Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-12 16:18:10 +0100

    Add files via upload
    preparations for the implementation of English

--- echoBot.py
+++ echoBot.py
@@ -14,45 +14,57 @@ def kor(tex):
     erg = [taul.get(w, w) for w in wo if w not in sen]
     return " ".join(erg)
 
-print("Hallo ich bin Echo")
-print("Wie geht es dir?")
-while True:
+print("Hello I'm Echo / Hallo ich bin Echo")
+print("Please choose your language / Bitte wähle deine Sprache")
+print("Please type in: / Bitte gebe ein: [English/Deutsch]")
+spr = input().upper()
+if "DE" in spr:
+
+    print("Wie geht es dir?")
+    while True:
     
-    st = str(input())
-    try:
-        strr = st.split() [-1]
-        if "fühle" in st or "fuehle" in st:
-            if "weil" in st:
-                tre = st.split("fühle") [0].strip()
-                print("Warum fühlst " + kor(tre) + "?")
-            else:
-                stre = st.split("fühle") [-1].strip()
-                print("Warum fühlst du " + kor(stre) + "?")
+        st = str(input())
+        try:
+            strr = st.split() [-1]
+            if "fühle" in st or "fuehle" in st:
+                if "weil" in st:
+                    tre = st.split("fühle") [0].strip()
+                    print("Warum fühlst " + kor(tre) + "?")
+                else:
+                    stre = st.split("fühle") [-1].strip()
+                    print("Warum fühlst du " + kor(stre) + "?")
         
         
-        elif "bin" in st:
-            if "weil" in st:
-                tre = st.split("bin") [0].strip()
-                print("Warum bist du " + kor(tre) + "?")
-            else:
-                stre = st.split("bin") [-1].strip()
-                print("Warum bist du " + kor(stre) + "?")
+            elif "bin" in st:
+                if "weil" in st:
+                    tre = st.split("bin") [0].strip()
+                    print("Warum bist du " + kor(tre) + "?")
+                else:
+                    stre = st.split("bin") [-1].strip()
+                    print("Warum bist du " + kor(stre) + "?")
             
-        else:
-            we = random.randint(0, 4)
-        
-            if we == 0:
-                print("Erzähl mir mehr")
-            elif we == 1:
-                print("Warum?")
-            elif we == 2:
-                print("Bist du dir sicher?")
-            elif we == 3:
-                print("Wieso denkst du das?")
             else:
-                print("Wenn du daran denkst - was kommt dir dann noch in den Kopf?")
+                if "gut" in st:
+                    print("Super! Was fühlst du sonst noch?")
+                    continue
+                    
+                we = random.randint(0, 4)
+        
+                if we == 0:
+                    print("Erzähl mir mehr")
+                elif we == 1:
+                    print("Warum?")
+                elif we == 2:
+                    print("Bist du dir sicher?")
+                elif we == 3:
+                    print("Wieso denkst du das?")
+                else:
+                    print("Wenn du daran denkst - was kommt dir dann noch in den Kopf?")
                 
-    except:
-        print("")
+        except:
+            print("")
+            
+else:
+    print("Please wait - your language will be available soon")
         
             
