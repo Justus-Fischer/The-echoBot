d985ec48b9740e11cbc64e22e7a2e08df69abcdf
Justus Fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-03-12 16:04:56 +0100

    Add files via upload
    update for the german version

--- echoBot-D.py
+++ echoBot-D.py
@@ -1,13 +1,43 @@
 import random
 
+def kor(tex):
+    taul = {
+        "bin" : "bist",
+        "mich" : "dich",
+        "mir" : "dir",
+        "ich" : "du",
+        "fühle" : "fühlst",
+        "fuehle" : "fühlst"
+    }
+    sen = ["weil"]
+    wo = tex.split() # .lower()?
+    erg = [taul.get(w, w) for w in wo if w not in sen]
+    return " ".join(erg)
+
+print("Hallo ich bin Echo")
 print("Wie geht es dir?")
 while True:
     
-    st = input()
+    st = str(input())
     try:
         strr = st.split() [-1]
-        if "fühle" in st or "fuehle" in st or "bin" in st:
-            print("Wieso bist du " + strr +"?")
+        if "fühle" in st or "fuehle" in st:
+            if "weil" in st:
+                tre = st.split("fühle") [0].strip()
+                print("Warum fühlst " + kor(tre) + "?")
+            else:
+                stre = st.split("fühle") [-1].strip()
+                print("Warum fühlst du " + kor(stre) + "?")
+        
+        
+        elif "bin" in st:
+            if "weil" in st:
+                tre = st.split("bin") [0].strip()
+                print("Warum bist du " + kor(tre) + "?")
+            else:
+                stre = st.split("bin") [-1].strip()
+                print("Warum bist du " + kor(stre) + "?")
+            
         else:
             we = random.randint(0, 4)
         
